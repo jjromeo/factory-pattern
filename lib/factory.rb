f08ec0fd8da350ae8854f3d6d537a29203d7dc4d
Jerome Pratt <jerome.pratt@outlook.com>
2014-09-01 11:32:38 +0100

    Refactoring the separate factory classes into a PartyFactory class

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,24 +1,25 @@
 class Party
 	attr_reader :members
 
-	def initialize(number)
+	def initialize(number, occupation)
 		@members = []
-		number.times { members << create}
+		number.times { members << create(occupation)}
 	end
 
 end
 
-class WarriorParty < Party
-	def create
-		Warrior.new
+class PartyFactory < Party 
+	def create(occupation)
+		if occupation == :warrior
+			Warrior.new
+		elsif occupation == :mage 
+			Mage.new
+		else
+			raise "Unknown hero type"
+		end
 	end
 end
 
-class MageParty < Party
-	def create
-		Mage.new
-	end
-end
 
 class Hero
 	attr_reader :damage, :abilities
